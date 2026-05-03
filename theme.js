32ea2cf29bfbefabc2cdcf577493796bd419c136
ahmad-ss <ahmadsetiyanto2597@gmail.com>
2026-05-02 23:09:47 -0700

    Remove portfolio page and associated assets, including images and styles

--- theme.js
+++
@@ -1,9 +0,0 @@
-// change link atribute when subject and body change value
-function generateSentto(){
-    const subject = document.getElementById('subject').value;
-    const body = document.getElementById('body').value;
-    const mailto = "mailto:ahmadsetiyono2597@gmail.com?subject="+subject+"&body="+body;
-    const element = document.getElementById('mailto');
-    element.setAttribute('href', mailto);
-    console.log(document.getElementById('mailto').getAttribute('href'));
-}
